dc441556022686948cd4e5dfc620920fa77ac274
meghna1542 <meghna.varshney_mca18@gla.ac.in>
2019-02-21 11:13:11 +0530

    give name

--- app/src/main/java/gla/gla1/MainActivity.java
+++ app/src/main/java/gla/gla1/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "meghna", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
